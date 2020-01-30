e4856186912ddbc218c68a27f0e55ac29f3160f1
Khalilurrehman <khalil_rehman1993@ymail.com>
2020-01-30 17:02:42 +0400

    Updates

---
+++ precache-manifest.fe4c5e352679bfd027b4d9cc277efd51.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f746578140cdae69bec5570303655bdb",
+    "url": "/Movie-Database/index.html"
+  },
+  {
+    "revision": "1aa913a358a30535453f",
+    "url": "/Movie-Database/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "a9676dfc157318b4a6f2",
+    "url": "/Movie-Database/static/css/main.07aeeee1.chunk.css"
+  },
+  {
+    "revision": "1aa913a358a30535453f",
+    "url": "/Movie-Database/static/js/2.ace8f929.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/Movie-Database/static/js/2.ace8f929.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a9676dfc157318b4a6f2",
+    "url": "/Movie-Database/static/js/main.cca5ccaa.chunk.js"
+  },
+  {
+    "revision": "d8377ff8d549f7db8038",
+    "url": "/Movie-Database/static/js/runtime-main.147c8478.js"
+  },
+  {
+    "revision": "c6b3a8d2a2424fdc4c8744017a9fe9ae",
+    "url": "/Movie-Database/static/media/no-image.c6b3a8d2.jpg"
+  }
+]);
